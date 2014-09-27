df0e171e6bd875b50b7312316244145687a3ac9c
Michael Whittaker <post@michael-whittaker.de>
2014-09-27 18:02:23 +0200

    adds the three university stations as db/seeds.rb. run `rake db:seed` to load

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Station.create(name: "Uni/Südring", query_name: "Uni Suedring", lat: 51.706283, long: 8.773098)
+Station.create(name: "Uni/Schöne Aussicht", query_name: "Uni Schoene Aussicht", lat: 51.710295, long: 8.77416)
+Station.create(name: "MuseumsForum", query_name: "Museumsforum", lat: 51.732142, long: 8.737064)
